f5a6a4d893e82dcaa1fd8db15303cc3cb7f2f951
lestermaudiel <flestermaudiel2130@gmail.com>
2023-08-24 21:26:32 -0600

    se creo la vista para menu index.

---
+++ views/menu/index.php
@@ -0,0 +1,4 @@
+<h1>menu principal</h1>
+<a href="/login_prueba/productos/datatable" class="btn btn-link">Datatable</a>
+<a href="/login_prueba/productos/estadistica" class="btn btn-link">Estadistica</a>
+<a href="/login_prueba/logout" class="btn btn-danger">Cerrar sesión</a>
